59004f2926cb7b0c3bba08f92e6c59520a85c04e
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-08-19 13:03:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 window.addEventListener("afterprint", (event) => {
     console.log("After print");
 });
-
-
-
- window.onafterprint = (event) => {
+window.onafterprint = (event) => {
     console.log("After print");
-  };
+};
+
+window.addEventListener("beforeprint", (event) => {
+    console.log("Before print");
+});
+
+window.onbeforeprint = (event) => {
+    console.log("Before print");
+};
 
 
 window.alert("Hello world!");
@@ -20,4 +25,28 @@ window.onappinstalled = () => {
 window.addEventListener("appinstalled", () => {
   console.log("Thank you for installing our app!");
 });
+
+
+// =====================================================
+const beforeUnloadListener = (event) => {
+    event.preventDefault();
+    return (event.returnValue = "");
+};
+
+const nameInput = document.querySelector("#name");
+
+nameInput.addEventListener("input", (event) => {
+    if (event.target.value !== "") {
+        addEventListener("beforeunload", beforeUnloadListener, { capture: true });
+    } else {
+        removeEventListener("beforeunload", beforeUnloadListener, {
+        capture: true,
+        });
+    }
+});
+//====================================================
+
+
+
+
 
