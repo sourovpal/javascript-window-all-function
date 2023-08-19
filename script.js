8732ab20ac30680185ad20463e29bd885b2a504d
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-08-19 12:53:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.addEventListener("afterprint", (event) => {
     console.log("After print");
 });
+
+
+
+ window.onafterprint = (event) => {
+    console.log("After print");
+  };
+
+
+window.alert("Hello world!");
+alert("Hello world!");
+
+window.onappinstalled = () => {
+  console.log("Thank you for installing our app!");
+};
+
+
+window.addEventListener("appinstalled", () => {
+  console.log("Thank you for installing our app!");
+});
+
